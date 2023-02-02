109fc666fdb89c47ad5a4e900fb2664d5c18b844
dianestar <dianestar@naver.com>
2023-02-02 09:39:43 +0900

    feat: 핵심간호술기영상 환자별 최대 3개로 변경

--- src/routes/CoreNursingSkillVideo/VideoForm.tsx
+++ src/routes/CoreNursingSkillVideo/VideoForm.tsx
@@ -15,50 +15,55 @@ interface Props {
 
 const VideoForm = (props: Props) => {
   const { totalSize, setTotalSize } = props;
-  const { student_uuid: user_id } = useUser();
+  // const { student_uuid: user_id } = useUser();
   const { patientInfo } = usePatient();
 
-  const [file, setFile] = useState<any>({ path: "", file: null });
+  const [files, setFiles] = useState<any[]>([{ path: "", file: null }, { path: "", file: null }, { path: "", file: null }]);
 
   return (
     <Table sx={{ width: "100%", marginTop: "24px" }} aria-label="simple table">
       <TableBody>
-        <TableRow
-          sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-        >
-          <TableCell sx={{ width: "30%" }}>
-            {patientInfo?.name}
-          </TableCell>
-          <TableCell sx={{ width: "60%"}}>
-            <MuiTextField
-              required={false}
-              type="file"
-              value={file.pat}
-              onChange={(e) => {
-                let newFile;
-                let size = 0;
-
-                const target = e.target as HTMLInputElement;
-                if (target && target.files) {
-                  newFile = { path: target.value, file: target.files[0] };
-                  size = target.files[0].size;
-                }
-
-                setTotalSize(Number((totalSize + size / Math.pow(1024, 3)).toFixed(4)));
-                setFile(newFile);
-              }}
-              InputProps={{ endAdornment: <FileIcon /> }}
-            />
-          </TableCell>
-          <TableCell sx={{ width: "10%" }}>
-            <Button variant="contained" size="small" onClick={() => {
-              setTotalSize(Number((totalSize - file.file.size / Math.pow(1024, 3)).toFixed(4)));
-              setFile({ path: "", file: null });
-            }}>
-              삭제
-            </Button>
-          </TableCell>
-        </TableRow>
+        {files.map((_, i) => (
+          <TableRow
+            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+          >
+            <TableCell sx={{ width: "30%"}}>
+              LEVEL {i+1}
+            </TableCell>
+            <TableCell sx={{ width: "30%" }}>
+              {patientInfo?.name}
+            </TableCell>
+            <TableCell sx={{ width: "30%"}}>
+              <MuiTextField
+                required={false}
+                type="file"
+                value={files[i].path}
+                onChange={(e) => {
+                  let newFiles = [...files];
+                  let size = 0;
+  
+                  const target = e.target as HTMLInputElement;
+                  if (target && target.files) {
+                    newFiles[i] = { path: target.value, file: target.files[0] };
+                    size = target.files[0].size;
+                  }
+  
+                  setTotalSize(Number((totalSize + size / Math.pow(1024, 3)).toFixed(4)));
+                  setFiles([...newFiles]);
+                }}
+                InputProps={{ endAdornment: <FileIcon /> }}
+              />
+            </TableCell>
+            <TableCell sx={{ width: "10%" }}>
+              <Button variant="contained" size="small" onClick={() => {
+                setTotalSize(Number((totalSize - files[i].file.size / Math.pow(1024, 3)).toFixed(4)));
+                setFiles([...files.slice(0, i), { path: "", file: null }, ...files.slice(i+1)]);
+              }}>
+                삭제
+              </Button>
+            </TableCell>
+          </TableRow>
+        ))}
       </TableBody>
     </Table>
   );
