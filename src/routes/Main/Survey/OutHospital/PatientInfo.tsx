ec7c7c37c930dd41b7114db686930610cbea1604
jeongseo <gldi1633@naver.com>
2023-04-26 18:38:54 +0900

    fix : 순서변경

--- src/routes/Main/Survey/OutHospital/PatientInfo.tsx
+++ src/routes/Main/Survey/OutHospital/PatientInfo.tsx
@@ -12,57 +12,62 @@ interface Props extends IFormRegister {
 
 const PatientInfo = (props: Props) => {
   const {
-    patientInfo: { patient_id, name, age, gender, ward, room, admin_hod, admin_pod, department, disease_main, main_doctor },
+    patientInfo: {
+      patient_id,
+      name,
+      age,
+      gender,
+      ward,
+      room,
+      admin_hod,
+      admin_pod,
+      department,
+      disease_main,
+      main_doctor,
+    },
     nurseName,
-    register
+    register,
   } = props;
 
   const infos = [
-    {title: "환자등록번호", value: patient_id},
-    {title: "환자명", value: name},
-    {title: "나이", value: age},
-    {title: "성별", value: gender},
-    {title: "병동", value: ward},
-    {title: "병실", value: room},
-    {title: "입원일", value: "in_date"},
-    {title: "퇴원일", value: "out_date"},
-    {title: "진료과", value: department},
-    {title: "진단명", value: disease_main.disease_kor},
-    {title: "담당 의사", value: main_doctor},
-    {title: "담당 간호사", value: nurseName},
+    { title: '환자명', value: name },
+    { title: '환자등록번호', value: patient_id },
+    { title: '나이', value: age },
+    { title: '성별', value: gender },
+    { title: '병동', value: ward },
+    { title: '병실', value: room },
+    { title: '입원일', value: 'in_date' },
+    { title: '퇴원일', value: 'out_date' },
+    { title: '진료과', value: department },
+    { title: '진단명', value: disease_main.disease_kor },
+    { title: '담당 의사', value: main_doctor },
+    { title: '담당 간호사', value: nurseName },
   ];
 
   return (
     <RowContainer xs={12}>
-      {infos.map(({ title, value }) => 
-        title === "성별"
-        ?
-        <RowContent title={title} titleRatio={1} childrenRatio={2}>
-          <Form.MuiRadioGroup
-            i18nKey="GENDER"
-            values={[1, 2]}
-            value={Number(value)}
-            defaultValue={Number(value)}
-          />
-        </RowContent>
-        :
-        (title === "입원일" || title === "퇴원일"
-        ?
-        <RowContent title={title} titleRatio={1} childrenRatio={2}>
-          <Form.MuiTextField
-            type="date"
-            {...register(`${value}`)}
-          />
-        </RowContent>
-        :
-        <RowContent title={title} titleRatio={1} childrenRatio={2}>
-          <Form.MuiTextField value={value} InputProps={{ readOnly: true }} />
-        </RowContent>
+      {infos.map(({ title, value }) =>
+        title === '성별' ? (
+          <RowContent title={title} titleRatio={1} childrenRatio={2}>
+            <Form.MuiRadioGroup
+              i18nKey="GENDER"
+              values={[1, 2]}
+              value={Number(value)}
+              defaultValue={Number(value)}
+            />
+          </RowContent>
+        ) : title === '입원일' || title === '퇴원일' ? (
+          <RowContent title={title} titleRatio={1} childrenRatio={2}>
+            <Form.MuiTextField type="date" {...register(`${value}`)} />
+          </RowContent>
+        ) : (
+          <RowContent title={title} titleRatio={1} childrenRatio={2}>
+            <Form.MuiTextField value={value} InputProps={{ readOnly: true }} />
+          </RowContent>
         )
       )}
     </RowContainer>
   );
-
 };
 
 export default PatientInfo;
