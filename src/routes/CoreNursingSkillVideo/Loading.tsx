a37a174fbd3acfabbd41256347d3d926fdb8b3e8
jeongseo <gldi1633@naver.com>
2023-05-08 10:13:22 +0900

    feat : 로딩 스피너 추가

--- src/routes/CoreNursingSkillVideo/Loading.tsx
+++ src/routes/CoreNursingSkillVideo/Loading.tsx
@@ -2,22 +2,31 @@ import Spinner from '../../assets/spinner.gif';
 
 export default () => {
   return (
-    <div style={{
-        position:'absolute',
-        maxWidth: '900px',
-        height: '400px',
+    <div
+      style={{
+        position: 'absolute',
+        backgroundColor: 'red',
+        marginTop: '50px',
+        width: '90%',
+        height: '100%',
         top: 0,
-        left:0,
+        left: 0,
         background: '#ffffffb7',
         zIndex: 999,
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'center',
         justifyContent: 'center',
-    }}>
-      <div style={{
-        font: "1rem' Noto Sans KR'",
-        textAlign: 'center'}}>잠시만 기다려 주세요.</div>
+      }}
+    >
+      <div
+        style={{
+          font: "1rem' Noto Sans KR'",
+          textAlign: 'center',
+        }}
+      >
+        잠시만 기다려 주세요.
+      </div>
       <img src={Spinner} alt="로딩중" width="5%" />
     </div>
   );
--- src/routes/CoreNursingSkillVideo/index.tsx
+++ src/routes/CoreNursingSkillVideo/index.tsx
@@ -1,12 +1,12 @@
 import { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import Loading from './Loading';
-import { Container } from "@mui/material";
+import { Container } from '@mui/material';
 
 import MuiDialog from 'components/MuiDialog';
 import {
   SurveyDialogProps,
-  TCoreNursingSkillVideoDefaultValues
+  TCoreNursingSkillVideoDefaultValues,
 } from 'routes/Main/Survey/type';
 import useSurvey from 'store/survey/useSurvey';
 import useNotification from 'hooks/useNotification';
@@ -19,29 +19,31 @@ import ProfModeInfo from './ProfModeInfo';
 
 import axios from 'axios';
 
-const CoreNursingSkillVideo = (props: SurveyDialogProps<TCoreNursingSkillVideoDefaultValues>) => {
+const CoreNursingSkillVideo = (
+  props: SurveyDialogProps<TCoreNursingSkillVideoDefaultValues>
+) => {
   const [refresh, setRefresh] = useState(false);
   const [totalSize, setTotalSize] = useState(0);
   const { isStudent } = useUser();
   const { student_uuid } = useStudent();
-  const [loading, setLoading]=useState(false);
+  const [loading, setLoading] = useState(false);
 
   const {
-      title,
-      isOpen,
-      disabled,
-      defaultValues,
-      user_id,
-      nurseName,
-      patientInfo,
-      onClose,
+    title,
+    isOpen,
+    disabled,
+    defaultValues,
+    user_id,
+    nurseName,
+    patientInfo,
+    onClose,
   } = props;
 
   const { onUpdateIsSave } = useSurvey();
   const { onSuccess, onFail, onResultCode, onRequired } = useNotification();
 
   const { handleSubmit, register, getValues, setValue, watch } = useForm({
-  defaultValues,
+    defaultValues,
   });
 
   const onSubmit = async (data: TCoreNursingSkillVideoDefaultValues) => {
@@ -50,9 +52,9 @@ const CoreNursingSkillVideo = (props: SurveyDialogProps<TCoreNursingSkillVideoDe
     let toSaveCount = 0;
     let lastSaveIdx = 0;
     setLoading(true);
-    console.log('시작지점', loading)
-    for (let i=0; i<3; i++) {
-      if (!files[i]["saved"] && files[i]["file"] !== null) {
+    console.log('시작지점', loading);
+    for (let i = 0; i < 3; i++) {
+      if (!files[i]['saved'] && files[i]['file'] !== null) {
         toSaveCount++;
         lastSaveIdx = i;
       }
@@ -60,51 +62,64 @@ const CoreNursingSkillVideo = (props: SurveyDialogProps<TCoreNursingSkillVideoDe
 
     if (toSaveCount === 0) {
       onUpdateIsSave(true);
-      setLoading(false)
-      onSuccess("핵심간호술기영상 저장에 성공하였습니다.");
-      console.log('로오딩', loading)
+      setLoading(false);
+      onSuccess('핵심간호술기영상 저장에 성공하였습니다.');
+      console.log('로오딩', loading);
       return;
     }
 
-    for (let i=0; i<3; i++) {
-      if (!files[i]["saved"] && files[i]["file"] !== null) {
+    for (let i = 0; i < 3; i++) {
+      if (!files[i]['saved'] && files[i]['file'] !== null) {
         const formData = new FormData();
-        formData.append("user_id", String(user_id));
-        formData.append("patient_id", String(patientInfo.patient_id));
-        formData.append("file", files[i]["file"]);
+        formData.append('user_id', String(user_id));
+        formData.append('patient_id', String(patientInfo.patient_id));
+        formData.append('file', files[i]['file']);
 
-        await axios.post(`${process.env.REACT_APP_API_URL}/file/video_upload`,
-          formData,
-          {
-            headers: {
-              Accept: 'application/json',
-              'Content-type': 'multipart/form-data;charset=UTF-8; boundary=6o2knFse3p53ty9dmcQvWAIx1zInP11uCfbm',
+        await axios
+          .post(
+            `${process.env.REACT_APP_API_URL}/file/video_upload`,
+            formData,
+            {
+              headers: {
+                Accept: 'application/json',
+                'Content-type':
+                  'multipart/form-data;charset=UTF-8; boundary=6o2knFse3p53ty9dmcQvWAIx1zInP11uCfbm',
+              },
             }
-          }
-        )
-        .then((res) => {
-          if (res.data.bucket_url === null) {
-            console.log(res);
-            onFail('핵심간호술기영상 저장에 실패하였습니다.', 'bucket_url is null');
-            return;
-          }
+          )
+          .then(res => {
+            if (res.data.bucket_url === null) {
+              console.log(res);
+              onFail(
+                '핵심간호술기영상 저장에 실패하였습니다.',
+                'bucket_url is null'
+              );
+              return;
+            }
 
-          if (i === lastSaveIdx) {
-            onUpdateIsSave(true);
-            setLoading(false)
-            onSuccess('핵심간호술기영상 저장에 성공하였습니다.');
-            console.log('로오딩', loading)
-
-            setRefresh(!refresh);
-            return;
-          }
-        })
-        .catch((e) => onFail('핵심간호술기영상 저장에 실패하였습니다.', e))
+            if (i === lastSaveIdx) {
+              onUpdateIsSave(true);
+              setLoading(false);
+              onSuccess('핵심간호술기영상 저장에 성공하였습니다.');
+              console.log('로오딩', loading);
+              setRefresh(!refresh);
+              return;
+            }
+          })
+          .catch(e => onFail('핵심간호술기영상 저장에 실패하였습니다.', e));
       }
     }
   };
 
-  const formProps = { disabled, watch, register, getValues, setValue, onSuccess, onRequired };
+  const formProps = {
+    disabled,
+    watch,
+    register,
+    getValues,
+    setValue,
+    onSuccess,
+    onRequired,
+  };
 
   return (
     <MuiDialog.SurveyForm
@@ -114,18 +129,29 @@ const CoreNursingSkillVideo = (props: SurveyDialogProps<TCoreNursingSkillVideoDe
       onSubmit={disabled ? undefined : handleSubmit(onSubmit)}
       update_at={defaultValues?.update_at}
     >
-      <Container maxWidth="md" sx={{ mt: 7.5, mb: 6 }}>
-        {isStudent 
-        ?
-        <StudentInfo totalSize={totalSize} />
-        :
-        <ProfModeInfo totalSize={totalSize} />
-        }
-        {loading ? <Loading /> : <VideoForm {...formProps} user_id={isStudent ? user_id : student_uuid} patient_id={patientInfo.patient_id} patient_name={patientInfo.name} totalSize={totalSize} setTotalSize={setTotalSize} refresh={refresh} />}
+      <Container maxWidth="md" sx={{ mt: 7.5, mb: 6, height: '300px' }}>
+        {isStudent ? (
+          <StudentInfo totalSize={totalSize} />
+        ) : (
+          <ProfModeInfo totalSize={totalSize} />
+        )}
+        {loading ? (
+          <Loading />
+        ) : (
+          <VideoForm
+            {...formProps}
+            user_id={isStudent ? user_id : student_uuid}
+            patient_id={patientInfo.patient_id}
+            patient_name={patientInfo.name}
+            totalSize={totalSize}
+            setTotalSize={setTotalSize}
+            refresh={refresh}
+          />
+        )}
         {/* <VideoForm {...formProps} user_id={isStudent ? user_id : student_uuid} patient_id={patientInfo.patient_id} patient_name={patientInfo.name} totalSize={totalSize} setTotalSize={setTotalSize} refresh={refresh} /> */}
       </Container>
     </MuiDialog.SurveyForm>
   );
-}
+};
 
 export default CoreNursingSkillVideo;
