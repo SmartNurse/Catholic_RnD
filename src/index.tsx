9f3c411b4dbc675ccc0f03896ed3ac1361d76f70
jeongseo <gldi1633@naver.com>
2023-05-22 10:32:45 +0900

    feat : ga 연동

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,9 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import ReactGA from 'react-ga';
+
+const TRACKING_ID: any = process.env.REACT_APP_GOOGLE_ANALYTICS_TRACKING_ID; // 발급받은 추적ID를 환경 변수로 불러온다.
+ReactGA.initialize(TRACKING_ID);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
