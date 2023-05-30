4f39dfdeae15af1482802d2e317f27e2c0c3016e
jeongseo <gldi1633@naver.com>
2023-05-30 12:03:09 +0900

    fix : 메뉴 이름 변경

--- src/routes/Main/MenuDrawer/MenuRecords.tsx
+++ src/routes/Main/MenuDrawer/MenuRecords.tsx
@@ -281,12 +281,12 @@ const MenuRecords = (props: Props) => {
     },
     {
       isPro: true,
-      label: '대장내시경',
+      label: '대장내시경 동의서',
       toggle: toggle.agreement,
     },
     {
       isPro: true,
-      label: '상부내시경',
+      label: '상부내시경 동의서',
       toggle: toggle.agreement,
     },
     {
