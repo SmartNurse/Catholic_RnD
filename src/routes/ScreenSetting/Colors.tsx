621fde1554c5f9a90a5ef30a4752657ff6960bd8
jeongseo <gldi1633@naver.com>
2023-05-30 10:13:53 +0900

    feat :  UIUX EMR 생성

--- src/routes/ScreenSetting/Colors.tsx
+++ src/routes/ScreenSetting/Colors.tsx
@@ -3,50 +3,55 @@ import { setLocalStorage } from 'utils/storage';
 import useNotification from 'hooks/useNotification';
 
 const Colors = () => {
-    const { onSuccess } = useNotification();
+  const { onSuccess } = useNotification();
 
-    const buttons = [
-        { param: "GREEN", text: "Standard Version Color", color: "#13AD74" },
-        { param: "BLUE", text: "Free Version Color", color: "#2264A8" },
-        { param: "RED", text: "Pro+ Version Color", color: "#AD4751" },
-        { param: "PURPLE", text: "Global Version Color", color: "#6A3A87" },
-        { param: "BLACK", text: "Night Duty Color", color: "#333333" },
-    ];
+  const buttons = [
+    { param: 'BLUE', text: 'BLUE', color: '#2264A8' },
 
-    return (
-        <Stack spacing={5}>
-            <Typography fontWeight="700" fontSize="14px" lineHeight="17.53px">화면 색상 변경</Typography>
-            {buttons.map(({ param, text, color }) => (
-                <Stack key={param} spacing={1}>
-                    <Typography>{param}</Typography>
-                    <Button
-                        size="large"
-                        variant="contained"
-                        sx={{
-                            ':hover': {
-                                color: `${color}`,
-                                backgroundColor: "white",
-                                border: `1px solid ${color}`
-                            },
-                            height: "36px",
-                            boxShadow: "0px 8px 8px 0px #0000001F",
-                            color: "white",
-                            backgroundColor: `${color}`,
-                        }}
-                        onClick={() => {
-                            setLocalStorage("theme_color", param);
-                            onSuccess(`화면 색상이 ${param}으로 변경되었습니다`);
-                            setTimeout(() => {
-                                window.location.replace("/");
-                            }, 1000);
-                        }}
-                    >
-                        {text}
-                    </Button>
-                </Stack>            
-            ))}
-        </Stack>
-    );
-}
+    { param: 'GREEN', text: 'GREEN', color: '#13AD74' },
+    { param: 'RED', text: 'RED', color: '#AD4751' },
+    { param: 'PURPLE', text: 'PURPLE', color: '#6A3A87' },
+    { param: 'BLACK', text: 'BLACK', color: '#333333' },
+  ];
+
+  return (
+    <>
+      <Typography fontWeight="700" fontSize="14px" lineHeight="17.53px">
+        화면 색상 변경
+      </Typography>
+      <Stack spacing={5} direction="row">
+        {buttons.map(({ param, text, color }) => (
+          <Stack key={param} spacing={1}>
+            <Button
+              size="large"
+              variant="contained"
+              sx={{
+                width: '100px',
+                ':hover': {
+                  color: `${color}`,
+                  backgroundColor: 'white',
+                  border: `1px solid ${color}`,
+                },
+                height: '100px',
+                boxShadow: '0px 8px 8px 0px #0000001F',
+                color: 'white',
+                backgroundColor: `${color}`,
+              }}
+              onClick={() => {
+                setLocalStorage('theme_color', param);
+                onSuccess(`화면 색상이 ${param}으로 변경되었습니다`);
+                setTimeout(() => {
+                  window.location.replace('/');
+                }, 1000);
+              }}
+            >
+              {text}
+            </Button>
+          </Stack>
+        ))}
+      </Stack>
+    </>
+  );
+};
 
 export default Colors;
--- src/routes/ScreenSetting/UIUX.tsx
+++ src/routes/ScreenSetting/UIUX.tsx
@@ -1,37 +1,312 @@
 import { Button, Stack, Typography } from '@mui/material';
-
-import { ReactComponent as Bestcare } from "assets/emr1.svg";
-import Nu from "assets/emr2.svg";
+import useNotification from 'hooks/useNotification';
+import { setLocalStorage } from 'utils/storage';
+import useUser from 'store/user/useUser';
+import useStudent from 'store/student/useStudent';
+import useSurvey from 'store/survey/useSurvey';
+import usePatient from 'store/patient/usePatient';
+import { ReactComponent as Bestcare } from 'assets/emr1.svg';
+import Nu from 'assets/emr2.svg';
 
 const UIUX = () => {
+  const { onSuccess } = useNotification();
+
+  const buttons = [
+    {
+      param: '가톨릭의료원 계열, 경희의료 중앙대의료원, 양지병원 등',
+      text: 'nU(Neuro Ubiquitous System)',
+      color: '#13AD74',
+    },
+    { param: '연세의료원', text: 'u-Severance 3.0', color: '#AD4751' },
+    { param: '삼성서울병원', text: '삼성SDS 다윈', color: '#6A3A87' },
+    { param: '한림대의료원', text: 'RefoMAX', color: '#333333' },
+  ];
+
   return (
-    <Stack spacing={5}>
-        <Stack spacing={1}>
-            <Bestcare />
-            <Typography>서울대병원, 길병원, 충북대병원, 제주대병원, 이화의료원, 근로복지공단, 국립대병원계, 동산병원 등</Typography>
+    <>
+      <Typography fontWeight="700" fontSize="14px" lineHeight="30px">
+        EMR 변경
+      </Typography>
+      <Stack spacing={5}>
+        <Stack
+          key={
+            '서울대병원, 길병원, 충북대병원, 제주대병원, 이화의료원, 근로복지공단, 국립대병원계, 동산병원'
+          }
+          spacing={1}
+        >
+          <Button
+            size="large"
+            variant="contained"
+            sx={{
+              width: '660px',
+              ':hover': {
+                color: `#2264A8`,
+                backgroundColor: 'white',
+                border: `1px solid #2264A8`,
+              },
+              height: '100px',
+              boxShadow: '0px 8px 8px 0px #0000001F',
+              color: 'black',
+              backgroundColor: `#EDF3FA`,
+              whiteSpace: 'pre',
+              fontSize: '19px',
+            }}
+            disabled={true}
+            onClick={() => {
+              setTimeout(() => {
+                window.location.href = `http://amisasan.s3-website.kr.object.ncloudstorage.com/#/`;
+              }, 1000);
+
+              setLocalStorage(
+                'theme_color',
+                '서울대병원, 길병원, 충북대병원, 제주대병원, 이화의료원, 근로복지공단, 국립대병원계, 동산병원'
+              );
+              onSuccess(`화면 EMR이 서울아산병원으로 변경되었습니다`);
+            }}
+          >
+            BESTCARE
+          </Button>
+          <Typography
+            sx={{
+              color: 'gray',
+              fontSize: '14px',
+              textAlign: 'center',
+              width: '660px',
+            }}
+          >
+            서울대병원, 길병원, 충북대병원, 제주대병원, 이화의료원,
+            근로복지공단, 국립대병원계, 동산병원
+            <br />
+            테스트중입니다.
+          </Typography>
         </Stack>
-        <Stack spacing={1}>
-            <img src={Nu} style={{ width: "73px", height: "34px" }} />
-            <Typography>가톨릭의료원 계열, 경희의료 중앙대의료원, 양지병원 등</Typography>
+
+        <Stack
+          key={'가톨릭의료원 계열, 경희의료 중앙대의료원, 양지병원 등'}
+          spacing={1}
+        >
+          <Button
+            variant="contained"
+            sx={{
+              width: '660px',
+              ':hover': {
+                color: `#2264A8`,
+                backgroundColor: 'white',
+                border: `1px solid #2264A8`,
+              },
+              height: '100px',
+              boxShadow: '0px 8px 8px 0px #0000001F',
+              color: 'black',
+              backgroundColor: `#EDF3FA`,
+              whiteSpace: 'pre',
+              fontSize: '19px',
+            }}
+            disabled={true}
+            onClick={() => {
+              setTimeout(() => {
+                window.location.href = `http://amisasan.s3-website.kr.object.ncloudstorage.com/#/`;
+              }, 1000);
+
+              setLocalStorage(
+                'theme_color',
+                '가톨릭의료원 계열, 경희의료 중앙대의료원, 양지병원 등'
+              );
+              onSuccess(`화면 EMR이 서울아산병원으로 변경되었습니다`);
+            }}
+          >
+            nU(Neuro Ubiquitous System)
+          </Button>
+          <Typography
+            sx={{
+              color: 'gray',
+              fontSize: '14px',
+              textAlign: 'center',
+              width: '660px',
+            }}
+          >
+            가톨릭의료원 계열, 경희의료 중앙대의료원, 양지병원 등
+            <br />
+            테스트중입니다.
+          </Typography>
         </Stack>
-        <Stack spacing={1}>
-            <Typography>u-Severance 3.0</Typography>
-            <Typography>연세의료원</Typography>
+
+        <Stack key={'연세의료원'} spacing={1}>
+          <Button
+            size="large"
+            variant="contained"
+            sx={{
+              width: '660px',
+              ':hover': {
+                color: `#2264A8`,
+                backgroundColor: 'white',
+                border: `1px solid #2264A8`,
+              },
+              height: '100px',
+              boxShadow: '0px 8px 8px 0px #0000001F',
+              color: 'black',
+              backgroundColor: `#EDF3FA`,
+              whiteSpace: 'pre',
+              fontSize: '19px',
+            }}
+            disabled={true}
+            onClick={() => {
+              setTimeout(() => {
+                window.location.href = `http://amisasan.s3-website.kr.object.ncloudstorage.com/#/`;
+              }, 1000);
+
+              setLocalStorage('theme_color', '연세의료원');
+              onSuccess(`화면 EMR이 서울아산병원으로 변경되었습니다`);
+            }}
+          >
+            u-Severance 3.0
+          </Button>
+          <Typography
+            sx={{
+              color: 'gray',
+              fontSize: '14px',
+              textAlign: 'center',
+              width: '660px',
+            }}
+          >
+            연세의료원
+            <br />
+            테스트중입니다.
+          </Typography>
         </Stack>
-        <Stack spacing={1}>
-            <Typography>삼성SDS 다윈</Typography>
-            <Typography>삼성서울병원</Typography>
+
+        <Stack key={'삼성서울병원'} spacing={1}>
+          <Button
+            size="large"
+            variant="contained"
+            sx={{
+              width: '660px',
+              ':hover': {
+                color: `#2264A8`,
+                backgroundColor: 'white',
+                border: `1px solid #2264A8`,
+              },
+              height: '100px',
+              boxShadow: '0px 8px 8px 0px #0000001F',
+              color: 'black',
+              backgroundColor: `#EDF3FA`,
+              whiteSpace: 'pre',
+              fontSize: '19px',
+            }}
+            disabled={true}
+            onClick={() => {
+              setTimeout(() => {
+                window.location.href = `http://amisasan.s3-website.kr.object.ncloudstorage.com/#/`;
+              }, 1000);
+
+              setLocalStorage('theme_color', '삼성서울병원');
+              onSuccess(`화면 EMR이 서울아산병원으로 변경되었습니다`);
+            }}
+          >
+            삼성SDS 다윈
+          </Button>
+          <Typography
+            sx={{
+              color: 'gray',
+              fontSize: '14px',
+              textAlign: 'center',
+              width: '660px',
+            }}
+          >
+            삼성서울병원
+            <br />
+            테스트중입니다.
+          </Typography>
         </Stack>
-        <Stack spacing={1}>
-            <Typography>AMIS 3.0</Typography>
-            <Typography>서울아산병원</Typography>
+
+        <Stack key={'서울아산병원'} spacing={1}>
+          <Button
+            size="large"
+            variant="contained"
+            sx={{
+              width: '660px',
+              ':hover': {
+                color: `#2264A8`,
+                backgroundColor: 'white',
+                border: `1px solid #2264A8`,
+              },
+              height: '100px',
+              boxShadow: '0px 8px 8px 0px #0000001F',
+              color: 'black',
+              backgroundColor: `#EDF3FA`,
+              whiteSpace: 'pre',
+              fontSize: '19px',
+            }}
+            onClick={() => {
+              setTimeout(() => {
+                window.open(
+                  'http://amisasan.s3-website.kr.object.ncloudstorage.com/#/'
+                );
+              }, 1000);
+
+              setLocalStorage('theme_color', '서울아산병원');
+              onSuccess(`화면 EMR이 서울아산병원으로 변경되었습니다`);
+            }}
+          >
+            AMIS 3.0
+          </Button>
+          <Typography
+            sx={{
+              color: 'gray',
+              fontSize: '14px',
+              textAlign: 'center',
+              width: '660px',
+            }}
+          >
+            서울아산병원
+          </Typography>
         </Stack>
-        <Stack spacing={1}>
-            <Typography>RefoMAX</Typography>
-            <Typography>한림대의료원</Typography>
+
+        <Stack key={'한림대의료원'} spacing={1}>
+          <Button
+            size="large"
+            variant="contained"
+            sx={{
+              width: '660px',
+              ':hover': {
+                color: `#2264A8`,
+                backgroundColor: 'white',
+                border: `1px solid #2264A8`,
+              },
+              height: '100px',
+              boxShadow: '0px 8px 8px 0px #0000001F',
+              color: 'black',
+              backgroundColor: `#EDF3FA`,
+              whiteSpace: 'pre',
+              fontSize: '19px',
+            }}
+            disabled={true}
+            onClick={() => {
+              setTimeout(() => {
+                window.location.href = `http://amisasan.s3-website.kr.object.ncloudstorage.com/#/`;
+              }, 1000);
+
+              setLocalStorage('theme_color', '한림대의료원');
+              onSuccess(`화면 EMR이 서울아산병원으로 변경되었습니다`);
+            }}
+          >
+            RefoMAX
+          </Button>
+          <Typography
+            sx={{
+              color: 'gray',
+              fontSize: '14px',
+              textAlign: 'center',
+              width: '660px',
+            }}
+          >
+            한림대의료원
+            <br />
+            테스트중입니다.
+          </Typography>
         </Stack>
-    </Stack>
+      </Stack>
+    </>
   );
-}
+};
 
 export default UIUX;
