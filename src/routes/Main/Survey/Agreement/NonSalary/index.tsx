084083a246a55646fde086c10bc456d878f8e596
jeongseo <gldi1633@naver.com>
2023-06-08 23:02:33 +0900

    fix : 테스트 중입니다 제거 및 alert창 변경

--- src/routes/Main/Survey/Agreement/NonSalary/index.tsx
+++ src/routes/Main/Survey/Agreement/NonSalary/index.tsx
@@ -79,10 +79,10 @@ const NonSalary = (props: SurveyDialogProps<TNonSalaryDefaultValues>) => {
         if (rc !== 1) return onResultCode(rc);
 
         onUpdateIsSave(true);
-        onSuccess('낙상 예방교육 확인서 저장에 성공하였습니다.');
+        onSuccess('비급여진료동의서 저장에 성공하였습니다.');
       })
       .catch(e => {
-        onFail('낙상 예방교육 확인서 저장에 실패하였습니다.', e);
+        onFail('비급여진료동의서 저장에 실패하였습니다.', e);
         console.log(e);
       });
   };
@@ -114,7 +114,6 @@ const NonSalary = (props: SurveyDialogProps<TNonSalaryDefaultValues>) => {
         }}
       >
         비급여진료동의서
-        <br />- 테스트 중 입니다-
       </Typography>
       <CommonPatientInfo patientInfo={patientInfo} nurseName={nurseName} />
       <Box sx={{ marginTop: '48px' }}>
