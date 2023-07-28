3f75914450eaa65555f98fb2d64b8a063eeda150
jeongseoe <gldi1633@naver.com>
2023-07-28 16:50:33 +0900

    fix  : 안쓰는거 제거하고 정리함

--- src/routes/Main/DisplayInformation/CollegePatientList.tsx
+++ src/routes/Main/DisplayInformation/CollegePatientList.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import { ListItemButton, Typography } from '@mui/material';
 import { getCollegePatientList } from 'apis/admin';
