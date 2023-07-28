a948f9557cad94e6c5ea92f5dad71258ad80f881
jeongseoe <gldi1633@naver.com>
2023-07-28 09:06:16 +0900

    fix : 줄맞춤

--- src/routes/Main/Survey/Safety/ConsequencesDetails.tsx
+++ src/routes/Main/Survey/Safety/ConsequencesDetails.tsx
@@ -14,72 +14,90 @@ const ConsequencesDetails = (props: Props) => {
 
   return (
     <>
-        <SectionTitle title="사고 발생 상황 및 결과" />
-        <RowContainer xs={12}>
-            <RowContent title="발생 일시" titleRatio={1} childrenRatio={11}>
-                <Form.MuiTextField
-                    required={false}
-                    fullWidth={false}
-                    type="date"
-                    disabled={disabled}
-                    {...register("accident_consequences_details.accidence_date")}
-                />
-            </RowContent>
-            <RowContent title="발견 일시" titleRatio={1} childrenRatio={11}>
-                <Form.MuiTextField
-                    fullWidth={false}
-                    type="date"
-                    disabled={disabled}
-                    {...register("accident_consequences_details.discovery_date")}
-                />
-            </RowContent>
-            <RowContent title="발생 장소" titleRatio={1} childrenRatio={11}>
-                <Form.MuiRadioGroup
-                    i18nKey="SAFETY.DISCOVERY.PLACE"
-                    values={[1, 2, 3, 4, 5, 0]}
-                    disabled={disabled}
-                    defaultValue={getValues('accident_consequences_details.discovery_place')}
-                    onChange={v => setValue('accident_consequences_details.discovery_place', v)}
-                    width="auto"
-                />
-                <Form.MuiTextField
-                    required={false}
-                    fullWidth={false}
-                    disabled={disabled}
-                    placeholder="직접 입력"
-                    sx={{ marginLeft: "10px" }}
-                    {...register('accident_consequences_details.discovery_place_etc')}
-                />
-            </RowContent>
-            <RowContent title="사고 유형" titleRatio={1} childrenRatio={11}>
-                <Form.MuiRadioGroup
-                    i18nKey="SAFETY.ACCIDENT.TYPE"
-                    values={[1, 2, 3]}
-                    disabled={disabled}
-                    defaultValue={getValues('accident_consequences_details.accident_type')}
-                    onChange={v => setValue('accident_consequences_details.accident_type', v)}
-                    width="auto"
-                />
-            </RowContent>
-            <RowContent title="사고 분류" titleRatio={1} childrenRatio={11}>
-                <Form.MuiRadioGroup
-                    i18nKey="SAFETY.ACCIDENT.CLASSIFICATION"
-                    values={[1, 2, 3, 4, 5, 6, 7, 8, 0]}
-                    disabled={disabled}
-                    defaultValue={getValues('accident_consequences_details.accident_classification')}
-                    onChange={v => setValue('accident_consequences_details.accident_classification', v)}
-                    width="auto"
-                />
-                <Form.MuiTextField
-                    required={false}
-                    fullWidth={false}
-                    disabled={disabled}
-                    placeholder="직접 입력"
-                    sx={{ marginLeft: "10px" }}
-                    {...register('accident_consequences_details.accident_classification_etc')}
-                />
-            </RowContent>
-        </RowContainer>
+      <SectionTitle title="사고 발생 상황 및 결과" />
+      <RowContainer xs={12}>
+        <RowContent title="발생 일시" titleRatio={1} childrenRatio={11}>
+          <Form.MuiTextField
+            required={false}
+            fullWidth={false}
+            type="date"
+            disabled={disabled}
+            {...register('accident_consequences_details.accidence_date')}
+          />
+        </RowContent>
+        <RowContent title="발견 일시" titleRatio={1} childrenRatio={11}>
+          <Form.MuiTextField
+            fullWidth={false}
+            required={false}
+            type="date"
+            disabled={disabled}
+            {...register('accident_consequences_details.discovery_date')}
+          />
+        </RowContent>
+        <RowContent title="발생 장소" titleRatio={1} childrenRatio={11}>
+          <Form.MuiRadioGroup
+            i18nKey="SAFETY.DISCOVERY.PLACE"
+            values={[1, 2, 3, 4, 5, 0]}
+            disabled={disabled}
+            defaultValue={getValues(
+              'accident_consequences_details.discovery_place'
+            )}
+            onChange={v =>
+              setValue('accident_consequences_details.discovery_place', v)
+            }
+            width="auto"
+          />
+          <Form.MuiTextField
+            required={false}
+            fullWidth={false}
+            disabled={disabled}
+            placeholder="직접 입력"
+            sx={{ marginLeft: '10px' }}
+            {...register('accident_consequences_details.discovery_place_etc')}
+          />
+        </RowContent>
+        <RowContent title="사고 유형" titleRatio={1} childrenRatio={11}>
+          <Form.MuiRadioGroup
+            i18nKey="SAFETY.ACCIDENT.TYPE"
+            values={[1, 2, 3]}
+            disabled={disabled}
+            defaultValue={getValues(
+              'accident_consequences_details.accident_type'
+            )}
+            onChange={v =>
+              setValue('accident_consequences_details.accident_type', v)
+            }
+            width="auto"
+          />
+        </RowContent>
+        <RowContent title="사고 분류" titleRatio={1} childrenRatio={11}>
+          <Form.MuiRadioGroup
+            i18nKey="SAFETY.ACCIDENT.CLASSIFICATION"
+            values={[1, 2, 3, 4, 5, 6, 7, 8, 0]}
+            disabled={disabled}
+            defaultValue={getValues(
+              'accident_consequences_details.accident_classification'
+            )}
+            onChange={v =>
+              setValue(
+                'accident_consequences_details.accident_classification',
+                v
+              )
+            }
+            width="auto"
+          />
+          <Form.MuiTextField
+            required={false}
+            fullWidth={false}
+            disabled={disabled}
+            placeholder="직접 입력"
+            sx={{ marginLeft: '10px' }}
+            {...register(
+              'accident_consequences_details.accident_classification_etc'
+            )}
+          />
+        </RowContent>
+      </RowContainer>
     </>
   );
 };
