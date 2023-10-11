ff7b3fcb901d55ef7728216fbba20685024663a4
jeongseoe <gldi1633@naver.com>
2023-10-11 15:44:15 +0900

    fix : 인쇄버튼 안보이게함

--- src/routes/CoreNursingSkillVideoExemple/index.tsx
+++ src/routes/CoreNursingSkillVideoExemple/index.tsx
@@ -1,12 +1,12 @@
 import { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 
-import { Container } from "@mui/material";
+import { Container } from '@mui/material';
 
 import MuiDialog from 'components/MuiDialog';
 import {
   SurveyDialogProps,
-  TCoreNursingSkillVideoDefaultValues as TCoreNursingSkillVideoDeExemplefaultValues
+  TCoreNursingSkillVideoDefaultValues as TCoreNursingSkillVideoDeExemplefaultValues,
 } from 'routes/Main/Survey/type';
 import useSurvey from 'store/survey/useSurvey';
 import useNotification from 'hooks/useNotification';
@@ -15,30 +15,23 @@ import useStudent from 'store/student/useStudent';
 
 import StudentModeInfoExemple from './StudentModeInfoExemple';
 
-
-const CoreNursingSkillVideoExemple = (props: SurveyDialogProps<TCoreNursingSkillVideoDeExemplefaultValues>) => {
+const CoreNursingSkillVideoExemple = (
+  props: SurveyDialogProps<TCoreNursingSkillVideoDeExemplefaultValues>
+) => {
   const [refresh, setRefresh] = useState(false);
   const [totalSize, setTotalSize] = useState(0);
   const { isStudent } = useUser();
   const { student_uuid } = useStudent();
 
-  const {
-      title,
-      isOpen,
-      onClose,
-  } = props;
+  const { title, isOpen, onClose } = props;
 
   return (
-    <MuiDialog.SurveyForm
-      title={title}
-      isOpen={isOpen}
-      onClose={onClose}
-    >
+    <MuiDialog.SurveyFormTwo title={title} isOpen={isOpen} onClose={onClose}>
       <Container sx={{ mt: 7.5, mb: 6 }}>
         <StudentModeInfoExemple totalSize={totalSize} />
       </Container>
-    </MuiDialog.SurveyForm>
+    </MuiDialog.SurveyFormTwo>
   );
-}
+};
 
 export default CoreNursingSkillVideoExemple;
