3d0938fd7d269dc2720d5cefb1fdb546a55484f1
jeongseoe <gldi1633@naver.com>
2023-10-26 16:09:20 +0900

    feat : 검색할 경우 string으로 type 변경하려고

--- src/utils/searchQuery.ts
+++ src/utils/searchQuery.ts
@@ -1,5 +1,9 @@
-import { parse } from 'query-string';
+import { parse, stringify } from 'query-string';
 
 export const getSearchQuery = (search: string) => {
   return parse(search);
+};
+
+export const setSearchQuery = (search: object) => {
+  return stringify(search);
 };
