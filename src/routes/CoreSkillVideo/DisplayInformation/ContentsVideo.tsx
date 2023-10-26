bec64d52b50a732390ea6c07afab53ea31ee51db
jeongseoe <gldi1633@naver.com>
2023-10-26 16:08:19 +0900

    feat : 핵심술기관리 contents 부분 컴포넌트 나눔

--- src/routes/CoreSkillVideo/DisplayInformation/ContentsVideo.tsx
+++ src/routes/CoreSkillVideo/DisplayInformation/ContentsVideo.tsx
@@ -1,9 +1,24 @@
 import { useEffect, useState, useCallback, useRef } from 'react';
+import { useLocation } from 'react-router-dom';
 
 import useUser from 'store/user/useUser';
 
 import { IPatientInfoSmall } from 'apis/admin/type';
 import { getSkillVideo } from 'apis/survey/index';
+import ContentsHead from './ContentsHead';
+import useNotification from 'hooks/useNotification';
+import ContentsBody from './ContentsBody';
+import useTable from 'hooks/useTable';
+import { IUpedateSkillVideo } from 'apis/survey/type';
+import { getSearchQuery } from 'utils/searchQuery';
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemButton,
+  ListSubheader,
+  Typography,
+} from '@mui/material';
 
 interface Props {
   patientInfo: IPatientInfoSmall;
@@ -11,7 +26,21 @@ interface Props {
 
 const ContentsVideo = ({ patientInfo }: Props) => {
   const { student_uuid: user_id } = useUser();
-  const [list, setList] = useState([]);
+  const { search } = useLocation();
+  const { onFail, onRequired } = useNotification();
+
+  const { page = 1, keyword, searchType } = getSearchQuery(search);
+  const {
+    list,
+    setList,
+    totalCount,
+    setTotalCount,
+    isLoading,
+    setIsLoading,
+    onPageChange,
+    selected,
+    setSelected,
+  } = useTable<IUpedateSkillVideo>();
 
   useEffect(() => {
     if (!patientInfo) return;
@@ -19,7 +48,7 @@ const ContentsVideo = ({ patientInfo }: Props) => {
     // 가상환자 상세정보 요청
     getSkillVideo({ patient_id: patientInfo.patient_id, user_id })
       .then(({ data }) => {
-        setList(data);
+        setList(data.student_info);
       })
       .catch(e => {
         setList([]);
@@ -30,7 +59,23 @@ const ContentsVideo = ({ patientInfo }: Props) => {
 
   console.log('list', list);
 
-  return <>ㅋㅋ</>;
+  return (
+    <Box flex={2} display="flex" flexDirection="column" overflow="auto">
+      <ContentsHead
+        onRequired={onRequired}
+        currentSearchType={searchType as any}
+      />
+      <ContentsBody
+        list={list}
+        isLoading={isLoading}
+        totalCount={totalCount}
+        page={Number(page) - 1}
+        onPageChange={onPageChange}
+        selected={selected}
+        onSelected={setSelected}
+      />
+    </Box>
+  );
 };
 
 export default ContentsVideo;
