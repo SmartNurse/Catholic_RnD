2e680b0112dae80206e66ab57b8cea1f16f50860
jeongseoe <gldi1633@naver.com>
2023-11-07 15:30:13 +0900

    feat: button event make & specify

--- src/routes/CoreSkillVideo/MenuDrawer/MenuRecords.tsx
+++ src/routes/CoreSkillVideo/MenuDrawer/MenuRecords.tsx
@@ -19,10 +19,16 @@ import { initialToggleObj } from './initialStates';
 interface Props {
   menuDrawerWidth: number;
   setMenuDrawerWidth: (menuDrawerWidth: number) => void;
+  menuState: string;
+  setMenuState: (menuState: string) => void;
 }
 
 const MenuRecords = (props: Props) => {
-  const { menuDrawerWidth, setMenuDrawerWidth } = props;
+  const { menuDrawerWidth, setMenuDrawerWidth, menuState, setMenuState } =
+    props;
+
+  const changeOriginMenu = () => setMenuState('핵심간호술기영상 관리');
+  const changeMenu = () => setMenuState('간호기록 전체조회');
 
   const [toggle, setToggle] = useState<IToggleObj>(initialToggleObj);
 
@@ -30,41 +36,33 @@ const MenuRecords = (props: Props) => {
     {
       icon: <NonStudentVideo />,
       label: '핵심간호술기영상 관리',
+      onclick: changeOriginMenu,
     },
     {
       icon: <NonStudentNursint />,
       label: '간호기록 전체조회',
+      onclick: changeMenu,
     },
   ];
 
   return (
     <Fragment>
-      {menus.map(({ icon, label }) => {
+      {menus.map(({ icon, label, onclick }) => {
         return (
           <>
             {menuDrawerWidth !== 220 ? (
               icon ? (
                 <ListItem key={label} disablePadding>
-                  <ListItemButton>
+                  <ListItemButton onClick={onclick}>
                     <ListItemIcon>{icon}</ListItemIcon>
                   </ListItemButton>
                 </ListItem>
               ) : null
             ) : icon ? (
               <ListItem key={label} disablePadding>
-                <ListItemButton>
+                <ListItemButton onClick={onclick}>
                   <ListItemIcon>{icon}</ListItemIcon>
                   <ListItemText primary={label} />
-                </ListItemButton>
-              </ListItem>
-            ) : toggle ? (
-              <ListItem key={label} disablePadding>
-                <ListItemButton className={''}>
-                  <ListItemIcon>{icon}</ListItemIcon>
-                  <ListItemText
-                    primary={label}
-                    sx={{ whiteSpace: 'pre-wrap' }}
-                  />
                 </ListItemButton>
               </ListItem>
             ) : (
