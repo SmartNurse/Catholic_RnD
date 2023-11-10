8579d4efdb8f584ff01ce9f2ce9e5de08132e1d4
jeongseoe <gldi1633@naver.com>
2023-11-10 11:24:47 +0900

    feat : file download make

--- src/routes/StudentManagement/DisplayInformation/VideoContents/ContentsBody.tsx
+++ src/routes/StudentManagement/DisplayInformation/VideoContents/ContentsBody.tsx
@@ -1,5 +1,6 @@
 import { GridColDef } from '@mui/x-data-grid';
-
+import { ReactComponent as FileIcon } from 'assets/file-icon.svg';
+import { Typography, Button, Stack } from '@mui/material';
 import MuiDataGrid from 'components/MuiDataGrid';
 import { IUpedateSkillVideo } from 'apis/survey/type';
 import { TGender } from 'apis/account/type';
@@ -18,15 +19,7 @@ interface Props {
 function ContentsBody(props: Props) {
   const i18n = useI18n();
 
-  const {
-    list,
-    isLoading,
-    totalCount,
-    page,
-    onPageChange,
-    onSelected,
-    selected,
-  } = props;
+  const { list, isLoading, totalCount, page, onPageChange } = props;
 
   const columns: GridColDef[] = [
     { field: 'student_id', headerName: '아이디(이메일)' },
@@ -39,33 +32,89 @@ function ContentsBody(props: Props) {
         i18n(`GENDER.${value}`),
     },
     {
-      field: 'student_video.student_info,nursery_video1',
+      field: 'student_video.nursery_video1',
       headerName: 'level 1',
-      valueFormatter: ({ value }) => (value ? value : '-'),
+      renderCell: list => {
+        if (
+          list.row.student_video === null ||
+          list.row.student_video.nursery_video1 === null
+        ) {
+          return <Typography>-</Typography>;
+        }
+        return (
+          <Stack direction="row">
+            <Typography sx={{ fontSize: '13px', lineHeight: '30px' }}>
+              {list.row.student_video.video_name1}
+            </Typography>
+
+            <Button
+              onClick={() => window.open(list.row.student_video.nursery_video1)}
+            >
+              <FileIcon />
+            </Button>
+          </Stack>
+        );
+      },
     },
     {
-      field: 'student_video.student_info,nursery_video2',
+      field: 'student_video.nursery_video2',
       headerName: 'level 2',
-      valueFormatter: ({ value }) => (value ? value : '-'),
+      renderCell: list => {
+        if (
+          list.row.student_video === null ||
+          list.row.student_video.nursery_video2 === null
+        ) {
+          return <Typography>-</Typography>;
+        }
+        return (
+          <Stack direction="row">
+            <Typography sx={{ fontSize: '13px', lineHeight: '30px' }}>
+              {list.row.student_video.video_name2}
+            </Typography>
+            <Button
+              onClick={() => window.open(list.row.student_video.nursery_video2)}
+            >
+              <FileIcon />
+            </Button>
+          </Stack>
+        );
+      },
     },
     {
-      field: 'student_video.student_info,nursery_video3',
+      field: 'student_video.nursery_video3',
       headerName: 'level 3',
-      valueFormatter: ({ value }) => (value ? value : '-'),
+      renderCell: list => {
+        if (
+          list.row.student_video === null ||
+          list.row.student_video.nursery_video3 === null
+        ) {
+          return <Typography>-</Typography>;
+        }
+        return (
+          <Stack direction="row">
+            <Typography sx={{ fontSize: '13px', lineHeight: '30px' }}>
+              {list.row.student_video.video_name3}
+            </Typography>
+            <Button
+              onClick={() => window.open(list.row.student_video.nursery_video3)}
+            >
+              <FileIcon />
+            </Button>
+          </Stack>
+        );
+      },
     },
   ];
-  console.log('body list : ', list);
+
   return (
     <MuiDataGrid
       columns={columns}
       loading={isLoading}
       onPageChange={onPageChange}
-      onSelectionModelChange={models => onSelected(models as string[])}
       page={page}
       pageSize={20}
       rows={list.map(item => ({ ...item, id: item.student_no }))}
       rowCount={totalCount}
-      selectionModel={selected}
     />
   );
 }
