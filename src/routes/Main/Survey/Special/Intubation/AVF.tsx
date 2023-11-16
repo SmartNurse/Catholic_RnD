4e5003ce0a91f94a8cc0233e53689277e0d0df61
jeongseoe <gldi1633@naver.com>
2023-11-16 15:00:12 +0900

    feat : 삽관기록지  뷰 완성

---
+++ src/routes/Main/Survey/Special/Intubation/AVF.tsx
@@ -0,0 +1,161 @@
+import { useEffect, useState } from 'react';
+
+import Form from 'components/Form';
+import { IFormRegister, IFormValues, IFormWatch } from 'routes/Main/type';
+
+import { Table, TableBody, TableCell, TableRow, Grid } from '@mui/material';
+import { AccessTime } from '@mui/icons-material';
+import { MobileTimePicker } from '@mui/x-date-pickers';
+
+import MuiTextField from 'components/Form/MuiTextField';
+import RowContainer from '../../components/RowContainer';
+import SectionTitle from '../../components/SectionTitle';
+import { IDialysisRecord } from 'apis/survey/type';
+
+interface Props extends IFormRegister, IFormValues, IFormWatch {
+  disabled?: boolean;
+}
+
+const AVF = (props: Props) => {
+  const { disabled, register, getValues, setValue, watch } = props;
+
+  const labels = ['삽입 부위', '삽입 상태'];
+  const registerIds = ['volume', 'blood_flow'];
+
+  const [dialysisRecord, setDialysisRecord] = useState<IDialysisRecord[]>([
+    {
+      time: '',
+      volume: '',
+      blood_flow: '',
+      arterial_pressure: '',
+      venous_pressure: '',
+      ufr: '',
+      tmp: '',
+      heparin: '',
+      sbp: '',
+      dbp: '',
+      pr: '',
+      bt: '',
+      rr: '',
+    },
+    {
+      time: '',
+      volume: '',
+      blood_flow: '',
+      arterial_pressure: '',
+      venous_pressure: '',
+      ufr: '',
+      tmp: '',
+      heparin: '',
+      sbp: '',
+      dbp: '',
+      pr: '',
+      bt: '',
+      rr: '',
+    },
+    {
+      time: '',
+      volume: '',
+      blood_flow: '',
+      arterial_pressure: '',
+      venous_pressure: '',
+      ufr: '',
+      tmp: '',
+      heparin: '',
+      sbp: '',
+      dbp: '',
+      pr: '',
+      bt: '',
+      rr: '',
+    },
+    {
+      time: '',
+      volume: '',
+      blood_flow: '',
+      arterial_pressure: '',
+      venous_pressure: '',
+      ufr: '',
+      tmp: '',
+      heparin: '',
+      sbp: '',
+      dbp: '',
+      pr: '',
+      bt: '',
+      rr: '',
+    },
+  ]);
+
+  useEffect(() => {
+    if (getValues('dialysis_db')) setDialysisRecord(getValues('dialysis_db'));
+    else setValue('dialysis_db', dialysisRecord);
+  }, []);
+
+  return (
+    <>
+      <SectionTitle title="AVF" />
+      <RowContainer xs={12}>
+        <Grid item flex={1}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table" size="small">
+            <TableBody>
+              <TableRow
+                key={'삽입 일시'}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  삽입 일시
+                </TableCell>
+                {Array(4)
+                  .fill(0)
+                  .map((_, idx) => (
+                    <TableCell key={idx}>
+                      <Form.MuiTextField
+                        type="date"
+                        disabled={disabled}
+                        // {...register(`${time}`)}
+                      />
+                    </TableCell>
+                  ))}
+              </TableRow>
+              {labels.map((label, labelIdx) => (
+                <TableRow
+                  key={label}
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row" sx={{ width: '150px' }}>
+                    {label}
+                  </TableCell>
+                  {Array(4)
+                    .fill(0)
+                    .map((_, idx) => (
+                      <TableCell key={idx}>
+                        <Form.MuiTextField
+                          value={
+                            dialysisRecord
+                              ? dialysisRecord[idx][registerIds[labelIdx]]
+                              : ''
+                          }
+                          onChange={e => {
+                            let newRecord = dialysisRecord
+                              ? [...dialysisRecord]
+                              : [];
+                            newRecord[idx][registerIds[labelIdx]] =
+                              e.target.value;
+                            setDialysisRecord(newRecord);
+                            setValue('dialysis_db', newRecord);
+                          }}
+                          required={false}
+                          disabled={disabled}
+                        />
+                      </TableCell>
+                    ))}
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </Grid>
+      </RowContainer>
+    </>
+  );
+};
+
+export default AVF;
---
+++ src/routes/Main/Survey/Special/Intubation/index.tsx
@@ -0,0 +1,153 @@
+import { useEffect, useState } from 'react';
+
+import { Grid, Typography } from '@mui/material';
+import { useForm } from 'react-hook-form';
+
+import MuiDialog from 'components/MuiDialog';
+import {
+  SurveyDialogProps,
+  TDialysisDefaultValues,
+} from 'routes/Main/Survey/type';
+import useSurvey from 'store/survey/useSurvey';
+import useNotification from 'hooks/useNotification';
+
+import { updateDialysis } from 'apis/survey';
+
+import CommonPatientInfo from '../../components/CommonPatientInfo';
+import IntubationC from './IntubationC';
+import IntubationA from './IntubationA';
+import FoleyCatheter from './FoleyCatheter';
+import AVF from './AVF';
+import HDCatheter from './HDCatheter';
+
+const Intubation = (props: SurveyDialogProps<TDialysisDefaultValues>) => {
+  const {
+    title,
+    isOpen,
+    disabled,
+    defaultValues,
+    user_id,
+    patientInfo,
+    nurseName,
+    onClose,
+  } = props;
+
+  const { onUpdateIsSave } = useSurvey();
+  const { onSuccess, onFail, onResultCode, onRequired } = useNotification();
+
+  const { handleSubmit, register, getValues, setValue, watch } = useForm({
+    defaultValues,
+  });
+
+  const onSubmit = (data: TDialysisDefaultValues) => {
+    const {
+      date,
+      time,
+      visiting_route,
+      visiting_route_etc,
+      dialysis_machine,
+      dialyzer,
+      dialysate,
+      vascular_access,
+      vascular_access_etc,
+      starting_nurse,
+      ending_nurse,
+      pre_previous_weight,
+      pre_today_weight,
+      pre_weight_change,
+      post_previous_weight,
+      post_today_weight,
+      post_weight_change,
+      dialysis_db,
+      additional_information,
+    } = data;
+
+    const request = {
+      user_id,
+      patient_id: patientInfo.patient_id,
+      hemodialysis_survey: {
+        dialysis_information: {
+          date,
+          time,
+          visiting_route:
+            visiting_route === '0' ? visiting_route_etc : visiting_route,
+          dialysis_machine,
+          dialyzer,
+          dialysate,
+          vascular_access:
+            vascular_access === '0' ? vascular_access_etc : vascular_access,
+          starting_nurse,
+          ending_nurse,
+        },
+        weight_information: {
+          pre_previous_weight,
+          pre_today_weight,
+          pre_weight_change,
+          post_previous_weight,
+          post_today_weight,
+          post_weight_change,
+        },
+        dialysis_db,
+        additional_information,
+      },
+    };
+
+    // console.log(request);
+    updateDialysis(request)
+      .then(({ data: { rc } }) => {
+        if (rc !== 1) return onResultCode(rc);
+
+        onUpdateIsSave(true);
+        onSuccess('삽관 기록지 저장에 성공하였습니다.');
+      })
+      .catch(e => onFail('삽관 기록지 저장에 실패하였습니다.', e));
+  };
+
+  const formProps = {
+    disabled,
+    watch,
+    register,
+    getValues,
+    setValue,
+    onSuccess,
+    onRequired,
+  };
+
+  return (
+    <MuiDialog.SurveyForm
+      title={title}
+      isOpen={isOpen}
+      onClose={onClose}
+      onSubmit={disabled ? undefined : handleSubmit(onSubmit)}
+      update_at={defaultValues?.update_at}
+    >
+      <Grid
+        container
+        wrap="wrap"
+        rowSpacing={5}
+        columnSpacing={3}
+        sx={{ py: 5, px: 1 }}
+      >
+        <Typography
+          sx={{
+            margin: '40px auto 0px auto',
+            fontWeight: '700',
+            fontSize: '16px',
+            textAlign: 'center',
+          }}
+        >
+          삽관 기록지
+          <br /> - 테스트중 입니다. -
+        </Typography>
+        <CommonPatientInfo patientInfo={patientInfo} nurseName={nurseName} />
+        <IntubationC {...formProps} />
+        <IntubationA {...formProps} />
+        <FoleyCatheter {...formProps} />
+        <AVF {...formProps} />
+        <HDCatheter {...formProps} />
+      </Grid>
+    </MuiDialog.SurveyForm>
+  );
+};
+
+export default Intubation;
