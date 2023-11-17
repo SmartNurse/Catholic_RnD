6ba77d005ad1298aaeb104387d9156d645bdc18b
jeongseoe <gldi1633@naver.com>
2023-11-17 14:57:15 +0900

    feat: Record type 원상태로 변경

--- src/routes/Main/Survey/type.ts
+++ src/routes/Main/Survey/type.ts
@@ -129,10 +129,10 @@ export enum MENU {
 export enum RECORD_TYPE {
   NANDA = '0',
   SOAPIE = '1',
-  ICNP = '2',
-  FOCUS_DAR = '3',
-  NARRATIVE_RECORD = '4',
-  REMARKS = '5',
+  ICNP = '5',
+  FOCUS_DAR = '2',
+  NARRATIVE_RECORD = '3',
+  REMARKS = '4',
 }
 
 export interface SurveyDialogProps<T>
