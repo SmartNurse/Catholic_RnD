69d8548a84b0e0210ed82435ee26a36b21ec9c0d
jeongseoe <gldi1633@naver.com>
2023-12-08 11:17:24 +0900

    test commit

--- src/routes/Main/index.tsx
+++ src/routes/Main/index.tsx
@@ -9,17 +9,24 @@ import InputInformation from './InputInformation';
 import MenuDrawer from './MenuDrawer';
 import Introduction from './Introduction';
 
-import { CoachMark, ICoachProps } from "react-coach-mark";
+import { CoachMark, ICoachProps } from 'react-coach-mark';
 import { Box, Typography, Stack, useTheme } from '@mui/material';
 
-import { greenTheme, blueTheme, redTheme, purpleTheme, blackTheme } from 'styles/theme';
+import {
+  greenTheme,
+  blueTheme,
+  redTheme,
+  purpleTheme,
+  blackTheme,
+} from 'styles/theme';
 
-import { ReactComponent as Number01 } from "assets/icon-number-01.svg";
-import { ReactComponent as Number02 } from "assets/icon-number-02.svg";
-import { ReactComponent as Number03 } from "assets/icon-number-03.svg";
-import { ReactComponent as Number04 } from "assets/icon-number-04.svg";
-import { ReactComponent as Number05 } from "assets/icon-number-05.svg";
+import { ReactComponent as Number01 } from 'assets/icon-number-01.svg';
+import { ReactComponent as Number02 } from 'assets/icon-number-02.svg';
+import { ReactComponent as Number03 } from 'assets/icon-number-03.svg';
+import { ReactComponent as Number04 } from 'assets/icon-number-04.svg';
+import { ReactComponent as Number05 } from 'assets/icon-number-05.svg';
 
+// 테스트 정서
 function Main() {
   const { palette } = useTheme();
 
@@ -43,7 +50,7 @@ function Main() {
   /* Introduction 관련 */
   const [open, setOpen] = useState(false);
   /********************/
-  
+
   /* 코치마크 관련 */
   const [show, setShow] = useState(false);
 
@@ -54,98 +61,105 @@ function Main() {
   const ref5 = useRef(null);
 
   const [activatedNumber, setActivateNumber] = useState<number>(0);
-  const NextButton = <button onClick={() => setActivateNumber(activatedNumber + 1)}>Next</button>;
+  const NextButton = (
+    <button onClick={() => setActivateNumber(activatedNumber + 1)}>Next</button>
+  );
 
   const coachList: Array<ICoachProps> = [
     {
-        activate: activatedNumber === 0,
-        component: 
-          <Stack direction="row" spacing={1} alignItems="center">
-            <Number01 fill={palette.primary.main} />
-            <Typography sx={{ display: "flex"}}>
-              <Typography sx={{ color: `${palette.primary.main}`}}>"환자 검색"</Typography>을 클릭해 가상환자를 선택해주세요!
+      activate: activatedNumber === 0,
+      component: (
+        <Stack direction="row" spacing={1} alignItems="center">
+          <Number01 fill={palette.primary.main} />
+          <Typography sx={{ display: 'flex' }}>
+            <Typography sx={{ color: `${palette.primary.main}` }}>
+              "환자 검색"
             </Typography>
-            {NextButton}
-          </Stack>,
-        reference: ref1,
-        tooltip: { position: 'bottom' }
+            을 클릭해 가상환자를 선택해주세요!
+          </Typography>
+          {NextButton}
+        </Stack>
+      ),
+      reference: ref1,
+      tooltip: { position: 'bottom' },
     },
     {
       activate: activatedNumber === 1,
-      component:
+      component: (
         <Stack direction="row" spacing={1} alignItems="center">
           <Number02 fill={palette.primary.main} />
-          <Typography sx={{ display: "flex"}}>
+          <Typography sx={{ display: 'flex' }}>
             환자 처방 내역과 다양한 기록지가 준비되어 있어요!
           </Typography>
           {NextButton}
-        </Stack>,
+        </Stack>
+      ),
       reference: ref2,
-      tooltip: { position: 'right' }
+      tooltip: { position: 'right' },
     },
     {
       activate: activatedNumber === 2,
-      component: 
+      component: (
         <Stack direction="row" spacing={1} alignItems="center">
           <Number03 fill={palette.primary.main} />
-          <Typography sx={{ display: "flex"}}>
-            <Typography sx={{ color: `${palette.primary.main}`}}>"V" 버튼</Typography>을 클릭하면 더 많은 메뉴가 보여요 확인해주세요!
+          <Typography sx={{ display: 'flex' }}>
+            <Typography sx={{ color: `${palette.primary.main}` }}>
+              "V" 버튼
+            </Typography>
+            을 클릭하면 더 많은 메뉴가 보여요 확인해주세요!
           </Typography>
           {NextButton}
-        </Stack>,
+        </Stack>
+      ),
       reference: ref3,
-      tooltip: { position: 'right' }
+      tooltip: { position: 'right' },
     },
     {
       activate: activatedNumber === 3,
-      component: 
+      component: (
         <Stack direction="row" spacing={0.5} alignItems="center">
           <Number04 fill={palette.primary.main} />
-          <Typography>
-            인수인계를 위한 간단한
+          <Typography>인수인계를 위한 간단한</Typography>
+          <Typography sx={{ color: `${palette.primary.main}` }}>
+            메모는
           </Typography>
-          <Typography sx={{ color: `${palette.primary.main}`}}>메모는</Typography>
-          <Typography>
-            이곳에!
-          </Typography>
+          <Typography>이곳에!</Typography>
           {NextButton}
-        </Stack>,
+        </Stack>
+      ),
       reference: ref4,
-      tooltip: { position: 'left' }
+      tooltip: { position: 'left' },
     },
     {
       activate: activatedNumber === 4,
-      component: 
+      component: (
         <Stack direction="row" spacing={1} alignItems="center">
           <Number05 fill={palette.primary.main} />
-          <Typography sx={{ display: "flex"}}>
+          <Typography sx={{ display: 'flex' }}>
             간호기록을 작성해볼까요?
-            <br/>
+            <br />
             우측 상단에 시간 선택을 한 후 기록을 입력하면 됩니다!
           </Typography>
           {NextButton}
-        </Stack>,
+        </Stack>
+      ),
       reference: ref5,
-      tooltip: { position: 'left' }
+      tooltip: { position: 'left' },
     },
- ]
+  ];
 
-  const coach : ICoachProps = coachList[activatedNumber];
+  const coach: ICoachProps = coachList[activatedNumber];
   /**************/
 
   /* 다시 보지 않기 체크박스 관련 */
   useEffect(() => {
-    if (getCookie("no_intro") !== "true") setOpen(true);
+    if (getCookie('no_intro') !== 'true') setOpen(true);
   }, []);
   /*************************/
 
   return (
     <>
-      <Introduction
-        open={open}
-        setOpen={setOpen}
-        setShow={setShow}
-      />
+      <Introduction open={open} setOpen={setOpen} setShow={setShow} />
       <Box display="flex" minWidth={1440}>
         <MenuDrawer
           name={`${student_name} ${title}`}
@@ -156,14 +170,8 @@ function Main() {
           coachRefA={ref2}
           coachRefB={ref3}
         />
-        <DisplayInformation
-          menuDrawerWidth={menuDrawerWidth}
-          coachRef={ref1}
-        />
-        <InputInformation
-          coachRefA={ref4}
-          coachRefB={ref5}
-        />
+        <DisplayInformation menuDrawerWidth={menuDrawerWidth} coachRef={ref1} />
+        <InputInformation coachRefA={ref4} coachRefB={ref5} />
       </Box>
       {show && <CoachMark {...coach} />}
     </>
